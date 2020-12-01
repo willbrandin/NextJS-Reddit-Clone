b01f4ac05f8f3bf70f7b6ab8051a9a1ee1eefe87
willbrandin <will@tidelaundry.com>
2020-12-01 08:28:32 -0700

    Auth routes/middleware

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
 import trim from "./middleware/trim";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 import authRoutes from "./routes/auth";
 
@@ -11,12 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
+app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("hello world"));
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, async () => {
+app.listen(process.env.PORT, async () => {
   console.log("Running on http://localhost:5000");
   try {
     await createConnection();
